480fdcda5a95c231351840478e35c5a203f02bb4
D1m1tri <enzosantanna00@gmail.com>
2023-10-24 10:35:01 -0300

    Aulas 1-10

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/empresa', function () {
+    return view('site.empresa');
+});
+
+Route::any('/any', function () {
+    return 'Permite todos os tipos de requisição HTTP (GET, POST, PUT, DELETE)';
+});
+
+Route::match(['get', 'post'], '/match', function () {
+    return 'Permite apenas os tipos de requisição HTTP (GET, POST)';
+});
+
+Route::get('/produto/{id}/{cat?}', function ($id, $cat = 'sem categoria') {
+    return 'O id do produto é: ' . $id . '<br> A categoria é: ' . $cat;
+});
