a2b7d80dbb5f24a223a972be7f0a73d65aeae73a
D1m1tri <enzosantanna00@gmail.com>
2023-10-25 11:04:14 -0300

    Aulas 15 e 16

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
 
-Route::get('/', [ProdutoController::class, 'index']);
+Route::resource('produtos', ProdutoController::class);
