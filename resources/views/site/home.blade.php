c45db5bc8fd2dad97f6ec8916f32472a2e84ea11
D1m1tri <enzosantanna00@gmail.com>
2023-10-27 16:23:09 -0300

    Aulas 36 a 41

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -4,10 +4,58 @@
 
 @section('conteudo')
 
-    {{-- Isso é um comentário --}}
-
-    {{-- isset($nome) ? $nome : 'não existe' --}}
-
-    {{ $teste ?? 'padrão' }}
+    <div class="row container">
+        <div class="col s12 m3">
+            <div class="card">
+                <div class="card-image">
+                    <img src="images/sample-1.jpg">
+                    <span class="card-title">Card Title</span>
+                    <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                </div>
+                <div class="card-content">
+                    <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col s12 m3">
+            <div class=card>
+                <div class=card-image>
+                    <img src="">
+                    <span class="card-title">Nome do Produto</span>
+                    <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add_shopping_cart</i></a>
+                </div>
+                <div class="card-content">
+                    <p>Descrição do Produto</p>
+                    <p>R$ 99,99</p>
+                </div>
+            </div>
+        </div>
+        <div class="col s12 m3">
+            <div class=card>
+                <div class=card-image>
+                    <img src="">
+                    <span class="card-title">Nome do Produto</span>
+                    <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add_shopping_cart</i></a>
+                </div>
+                <div class="card-content">
+                    <p>Descrição do Produto</p>
+                    <p>R$ 99,99</p>
+                </div>
+            </div>
+        </div>
+        <div class="col s12 m3">
+            <div class=card>
+                <div class=card-image>
+                    <img src="">
+                    <span class="card-title">Nome do Produto</span>
+                    <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add_shopping_cart</i></a>
+                </div>
+                <div class="card-content">
+                    <p>Descrição do Produto</p>
+                    <p>R$ 99,99</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -7,10 +7,25 @@
         <title>
             Curso Laravel 9 | @yield('titulo')
         </title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     </head>
 
 
     <body>
+        <nav class="red">
+            <div class="nav-wrapper container">
+                <a href="#" class="brand-logo center">Curso Laravel 9</a>
+                <ul id="nav-mobile" class="left">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Carrinho</a></li>
+                </ul>
+            </div>
+        </nav>
+
+
         @yield('conteudo')
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     </body>
 </html>
